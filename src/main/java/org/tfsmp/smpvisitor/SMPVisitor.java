0567381ef3cfdda3562816d285c87c3ef29df922
Super_ <awesomegamer156@gmail.com>
2020-06-24 19:13:10 -0400

    init

---
+++ src/main/java/org/tfsmp/smpvisitor/SMPVisitor.java
@@ -0,0 +1,16 @@
+package org.tfsmp.smpvisitor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPVisitor extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+    }
+
+    @Override
+    public void onDisable()
+    {
+    }
+}
